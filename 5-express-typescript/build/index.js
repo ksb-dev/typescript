369e25160f9958d7eb8b3c4c812bebcb9c190acf
Kedar Babaleshwar <babaleshwarkedar@gmail.com>
2023-05-25 06:23:10 +0530

    Initial Commit

--- 5-express-typescript/build/index.js
+++ 5-express-typescript/build/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('Hi');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const login_1 = require("./routes/login");
+const body_parser_1 = __importDefault(require("body-parser"));
+const cookie_session_1 = __importDefault(require("cookie-session"));
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use((0, cookie_session_1.default)({ keys: ['asyasyu'] }));
+app.use(login_1.router);
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
--- 5-express-typescript/src/index.ts
+++ 5-express-typescript/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hi')
+import express from 'express'
+import { router } from './routes/login'
+import bodyParser from 'body-parser'
+import cookieSession from 'cookie-session'
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieSession({ keys: ['asyasyu'] }))
+app.use(router)
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
